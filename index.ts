23e3c84f40ac57256353661b2a82b208455fd242
linlexing <linlexing@gmail.com>
2018-05-14 14:28:32 +0800

    根据提示改正警告

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosPromise } from 'axios';
 export function APIGet<T>(eleName: string, methodName: string, signStr: string | null,
 	param: any): AxiosPromise<T> {
-	let header: any = { "dbweb-api": methodName };
-	let params :any ={};
+	const header: any = { "dbweb-api": methodName };
+	const params :any ={};
 	if(param){
 		params._p = JSON.stringify(param);
 	}
@@ -11,18 +11,18 @@ export function APIGet<T>(eleName: string, methodName: string, signStr: string |
 	}
 	return axios.get("/" + eleName, {
 		headers: header,
-		params: params
+		params
 	});
 }
 
 export function APIPost<T>(eleName: string, methodName: string, signStr: string,
 	param: any): AxiosPromise<T> {
-	let header: any = { "dbweb-api": methodName };
+	const header: any = { "dbweb-api": methodName };
 	if (signStr != null) {
 		header._s = signStr;
 	}
 	return axios.post("/" + eleName, {
-		headers: header,
-		data: param
+		data: param,
+		headers: header
 	});
 }
