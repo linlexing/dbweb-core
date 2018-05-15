6a868feb57e3f3cf84e459c87029443ba8d84e7c
linlexing <linlexing@gmail.com>
2018-05-15 17:59:12 +0800

    增加rootpath

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 import axios, { AxiosPromise } from 'axios';
+import { join } from 'path';
+let RootPath: string = "/";
+export function SetRootPath(val: string): void {
+	RootPath = val;
+}
 export function APIGet<T>(eleName: string, methodName: string, signStr: string | null,
 	param: any): AxiosPromise<T> {
 	const header: any = { "dbweb-api": methodName };
-	const params :any ={};
-	if(param){
+	const params: any = {};
+	if (param) {
 		params._p = JSON.stringify(param);
 	}
 	if (signStr != null) {
 		header._s = signStr;
 	}
-	return axios.get("/" + eleName, {
+	const strURL = join(RootPath, eleName);
+	return axios.get(strURL, {
 		headers: header,
 		params
 	});
@@ -21,7 +27,7 @@ export function APIPost<T>(eleName: string, methodName: string, signStr: string,
 	if (signStr != null) {
 		header._s = signStr;
 	}
-	return axios.post("/" + eleName, {
+	return axios.post(join(RootPath, eleName), {
 		data: param,
 		headers: header
 	});
