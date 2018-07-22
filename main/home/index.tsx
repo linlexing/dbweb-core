24c12ac67ecf503f058210fa176c3223101c6a38
linlexing <linlexing@gmail.com>
2018-07-22 16:27:34 +0800

    修正内容布局宽度不正确的bug

--- main/home/index.tsx
+++ main/home/index.tsx
@@ -1,22 +1,22 @@
 // tslint:disable:no-console
 import {
-    AppBar,
-    Badge,
-    ClickAwayListener,
-    Divider,
-    Drawer,
-    Grow,
-    Icon,
-    IconButton,
-    ListItemIcon,
-    ListItemText,
-    MenuItem,
-    MenuList,
-    Paper,
-    Toolbar,
-    Tooltip,
-    Typography,
-    WithStyles
+	AppBar,
+	Badge,
+	ClickAwayListener,
+	Divider,
+	Drawer,
+	Grow,
+	Icon,
+	IconButton,
+	ListItemIcon,
+	ListItemText,
+	MenuItem,
+	MenuList,
+	Paper,
+	Toolbar,
+	Tooltip,
+	Typography,
+	WithStyles
 } from '@material-ui/core';
 import withStyles from '@material-ui/core/styles/withStyles';
 import * as classNames from 'classnames';
@@ -40,232 +40,232 @@ import { IHomeStore } from './reducer';
 import { clsNames, styles } from './style';
 
 interface IHomeEvents {
-    openMenu: () => any;
-    hideMenu: () => any;
-    toggleUserMenu: (open: boolean) => any;
+	openMenu: () => any;
+	hideMenu: () => any;
+	toggleUserMenu: (open: boolean) => any;
 }
 interface IHomeProps extends IHomeEvents, IHomeStore, RouteComponentProps<any>, WithStyles<clsNames> {
-    projectLabel: string;
-    menuOpen: boolean;
-    publicEles: IElement[];
-    elements: IElement[];
-    userName: string;
-    dept: IDept;
-    toRootDept: IDept[];
-    nextLevelDept: IDept[];
-    serviceVersion: number;
-    brand: string;
-    selElement: IElement;
+	projectLabel: string;
+	menuOpen: boolean;
+	publicEles: IElement[];
+	elements: IElement[];
+	userName: string;
+	dept: IDept;
+	toRootDept: IDept[];
+	nextLevelDept: IDept[];
+	serviceVersion: number;
+	brand: string;
+	selElement: IElement;
 }
 const mapStateToProps = (state: any) => ({
-    ...state.home,
-    userName: state.root.userName,
-    elements: state.root.elements,
-    publicEles: state.root.publicEles,
-    projectLabel: state.root.projectLabel,
-    dept: state.root.dept,
-    brand: state.root.brand,
-    serviceVersion: state.root.serviceVersion,
-    toRootDept: state.root.toRootDept,
-    nextLevelDept: state.root.nextLevelDept
+	...state.home,
+	userName: state.root.userName,
+	elements: state.root.elements,
+	publicEles: state.root.publicEles,
+	projectLabel: state.root.projectLabel,
+	dept: state.root.dept,
+	brand: state.root.brand,
+	serviceVersion: state.root.serviceVersion,
+	toRootDept: state.root.toRootDept,
+	nextLevelDept: state.root.nextLevelDept
 });
 const mapDispatchToProps = {
-    openMenu: actions.doOpenMenu,
-    hideMenu: actions.doHideMenu,
-    toggleUserMenu: actions.doToggleUserMenu
+	openMenu: actions.doOpenMenu,
+	hideMenu: actions.doHideMenu,
+	toggleUserMenu: actions.doToggleUserMenu
 };
 
 class Home extends React.PureComponent<IHomeProps> {
-    private target: React.RefObject<HTMLDivElement>;
-    constructor(props: IHomeProps) {
-        super(props);
-        this.hideUserMenu = this.hideUserMenu.bind(this);
-        this.toggleUserMenu = this.toggleUserMenu.bind(this);
+	private target: React.RefObject<HTMLDivElement>;
+	constructor(props: IHomeProps) {
+		super(props);
+		this.hideUserMenu = this.hideUserMenu.bind(this);
+		this.toggleUserMenu = this.toggleUserMenu.bind(this);
 
-        this.logout = this.logout.bind(this);
-        this.target = React.createRef();
-    }
-    public render() {
-        const {
-            menuOpen,
-            userMenuOpen,
-            classes,
-            theme,
-            elements,
-            publicEles,
-            projectLabel,
-            openMenu,
-            hideMenu,
-            userName,
-            dept,
-            brand,
-            serviceVersion,
-            nextLevelDept,
-            toRootDept
-        } = this.props;
-        const selEleName = location.pathname.split('/');
-        let selEle;
-        // let userBtn;
-        // if (this.userButton.current) {
-        //     userBtn = this.userButton.current;
-        // }
-        if (selEleName.length > 2) {
-            selEle = _.find(elements, { Name: decodeURIComponent(selEleName[2]) });
-            if (!selEle) {
-                selEle = _.find(publicEles, { Name: decodeURIComponent(selEleName[2]) });
-            }
-        }
-        return (
-            <div className={classes.appFrame}>
-                <AppBar
-                    className={classNames(classes.appBar, {
-                        [classes.appBarShift]: menuOpen
-                    })}>
-                    <Toolbar disableGutters={!menuOpen}>
-                        <IconButton
-                            color="inherit"
-                            aria-label="open drawer"
-                            onClick={openMenu}
-                            className={classNames(classes.menuButton, menuOpen && classes.hide)}>
-                            <Icon>menu</Icon>
-                        </IconButton>
-                        <Typography variant="title" color="inherit" noWrap={true} className={classes.toolbarText}>
-                            {selEle ? clearText(selEle.Label) : 'not found'}
-                        </Typography>
+		this.logout = this.logout.bind(this);
+		this.target = React.createRef();
+	}
+	public render() {
+		const {
+			menuOpen,
+			userMenuOpen,
+			classes,
+			theme,
+			elements,
+			publicEles,
+			projectLabel,
+			openMenu,
+			hideMenu,
+			userName,
+			dept,
+			brand,
+			serviceVersion,
+			nextLevelDept,
+			toRootDept
+		} = this.props;
+		const selEleName = location.pathname.split('/');
+		let selEle;
+		// let userBtn;
+		// if (this.userButton.current) {
+		//     userBtn = this.userButton.current;
+		// }
+		if (selEleName.length > 2) {
+			selEle = _.find(elements, { Name: decodeURIComponent(selEleName[2]) });
+			if (!selEle) {
+				selEle = _.find(publicEles, { Name: decodeURIComponent(selEleName[2]) });
+			}
+		}
+		return (
+			<div className={classes.appFrame}>
+				<AppBar
+					className={classNames(classes.appBar, {
+						[classes.appBarShift]: menuOpen
+					})}>
+					<Toolbar disableGutters={!menuOpen}>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							onClick={openMenu}
+							className={classNames(classes.menuButton, menuOpen && classes.hide)}>
+							<Icon>menu</Icon>
+						</IconButton>
+						<Typography variant="title" color="inherit" noWrap={true} className={classes.toolbarText}>
+							{selEle ? clearText(selEle.Label) : 'not found'}
+						</Typography>
 
-                        <Manager>
-                            <Target>
-                                <div ref={this.target}>
-                                    <Tooltip title={userName + ' ' + dept.Code + '.' + dept.Name}>
-                                        <IconButton color="inherit" onClick={this.toggleUserMenu}>
-                                            <Icon>account_circle</Icon>
-                                        </IconButton>
-                                    </Tooltip>
-                                </div>
-                            </Target>
-                            <Popper
-                                placement="bottom-end"
-                                eventsEnabled={userMenuOpen}
-                                style={{ pointerEvents: userMenuOpen ? 'auto' : 'none' }}>
-                                <ClickAwayListener onClickAway={this.hideUserMenu}>
-                                    <Grow in={userMenuOpen} style={{ transformOrigin: '0 0 0' }}>
-                                        <Paper id="menu-list-collapse">
-                                            <MenuList role="menu">
-                                                <MenuItem onClick={this.logout}>
-                                                    <ListItemIcon>
-                                                        <Icon>exit_to_app</Icon>
-                                                    </ListItemIcon>
-                                                    <ListItemText primary="退出系统" inset={true} />
-                                                </MenuItem>
-                                                {nextLevelDept.map(val => <DeptList key={val.Code} dept={val} />)}
-                                                {toRootDept.length > 0 ? <Divider /> : null}
-                                                {toRootDept.length > 0
-                                                    ? toRootDept.map(val => <DeptList key={val.Code} dept={val} />)
-                                                    : null}
-                                            </MenuList>
-                                        </Paper>
-                                    </Grow>
-                                </ClickAwayListener>
-                            </Popper>
-                        </Manager>
-                        <IconButton color="inherit" style={{ marginRight: menuOpen ? 0 : 24 }}>
-                            <Badge badgeContent={4} color="secondary">
-                                <Icon>notifications</Icon>
-                            </Badge>
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    variant="persistent"
-                    anchor="left"
-                    open={menuOpen}
-                    classes={{
-                        paper: classes.drawerPaper
-                    }}>
-                    <div className={classes.drawerHeader}>
-                        <div
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                paddingLeft: 20,
-                                color: theme ? theme.palette.grey['600'] : undefined
-                            }}>
-                            <Typography
-                                variant="title"
-                                color="inherit"
-                                noWrap={true}
-                                style={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    justifyItems: 'center',
-                                    marginBottom: 4,
-                                    fontWeight: theme ? theme.typography.fontWeightRegular : undefined
-                                }}>
-                                <Icon>{brand}</Icon>
-                                <span>{projectLabel}</span>
-                            </Typography>
-                            <Typography variant="caption" color="inherit" noWrap={true}>
-                                版本:<Link
-                                    style={{ textDecoration: 'none', color: 'inherit' }}
-                                    to={elementRouterURL('version')}>
-                                    {serviceVersion}
-                                </Link>
-                            </Typography>
-                        </div>
-                        <IconButton onClick={hideMenu} style={{ color: theme && theme.palette.grey['500'] }}>
-                            {theme && theme.direction === 'rtl' ? (
-                                <Icon>chevron_right</Icon>
-                            ) : (
-                                <Icon>chevron_left</Icon>
-                            )}
-                        </IconButton>
-                    </div>
-                    <Divider />
-                    <Menus />
-                </Drawer>
-                <main
-                    className={classNames(classes.content, classes[`content-left`], {
-                        [classes.contentShift]: menuOpen,
-                        [classes['contentShift-left']]: menuOpen
-                    })}>
-                    <div className={classes['content-top']} />
-                    <Switch>
-                        {elements &&
-                            _.union(publicEles, elements).map<JSX.Element>(
-                                (val): any => {
-                                    return (
-                                        <Route key={val.Name} path={'/front/' + val.Name}>
-                                            <MainComponent element={val} />
-                                        </Route>
-                                    );
-                                }
-                            )}
-                        <Route key="not found" component={NotFound} />
-                    </Switch>
-                </main>
-            </div>
-        );
-    }
-    private hideUserMenu(event: any) {
-        if (this.target.current && this.target.current.contains(event.target)) {
-            return;
-        }
-        if (this.props.userMenuOpen) {
-            this.props.toggleUserMenu(false);
-        }
-    }
-    private toggleUserMenu() {
-        this.props.toggleUserMenu(!this.props.userMenuOpen);
-    }
-    private logout() {
-        this.props.toggleUserMenu(false);
-        logout();
-    }
+						<Manager>
+							<Target>
+								<div ref={this.target}>
+									<Tooltip title={userName + ' ' + dept.Code + '.' + dept.Name}>
+										<IconButton color="inherit" onClick={this.toggleUserMenu}>
+											<Icon>account_circle</Icon>
+										</IconButton>
+									</Tooltip>
+								</div>
+							</Target>
+							<Popper
+								placement="bottom-end"
+								eventsEnabled={userMenuOpen}
+								style={{ pointerEvents: userMenuOpen ? 'auto' : 'none' }}>
+								<ClickAwayListener onClickAway={this.hideUserMenu}>
+									<Grow in={userMenuOpen} style={{ transformOrigin: '0 0 0' }}>
+										<Paper id="menu-list-collapse">
+											<MenuList role="menu">
+												<MenuItem onClick={this.logout}>
+													<ListItemIcon>
+														<Icon>exit_to_app</Icon>
+													</ListItemIcon>
+													<ListItemText primary="退出系统" inset={true} />
+												</MenuItem>
+												{nextLevelDept.map(val => <DeptList key={val.Code} dept={val} />)}
+												{toRootDept.length > 0 ? <Divider /> : null}
+												{toRootDept.length > 0
+													? toRootDept.map(val => <DeptList key={val.Code} dept={val} />)
+													: null}
+											</MenuList>
+										</Paper>
+									</Grow>
+								</ClickAwayListener>
+							</Popper>
+						</Manager>
+						<IconButton color="inherit" style={{ marginRight: menuOpen ? 0 : 24 }}>
+							<Badge badgeContent={4} color="secondary">
+								<Icon>notifications</Icon>
+							</Badge>
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					variant="persistent"
+					anchor="left"
+					open={menuOpen}
+					classes={{
+						paper: classes.drawerPaper
+					}}>
+					<div className={classes.drawerHeader}>
+						<div
+							style={{
+								display: 'flex',
+								flexDirection: 'column',
+								paddingLeft: 20,
+								color: theme ? theme.palette.grey['600'] : undefined
+							}}>
+							<Typography
+								variant="title"
+								color="inherit"
+								noWrap={true}
+								style={{
+									display: 'flex',
+									flexDirection: 'row',
+									justifyItems: 'center',
+									marginBottom: 4,
+									fontWeight: theme ? theme.typography.fontWeightRegular : undefined
+								}}>
+								<Icon>{brand}</Icon>
+								<span>{projectLabel}</span>
+							</Typography>
+							<Typography variant="caption" color="inherit" noWrap={true}>
+								版本:<Link
+									style={{ textDecoration: 'none', color: 'inherit' }}
+									to={elementRouterURL('version')}>
+									{serviceVersion}
+								</Link>
+							</Typography>
+						</div>
+						<IconButton onClick={hideMenu} style={{ color: theme && theme.palette.grey['500'] }}>
+							{theme && theme.direction === 'rtl' ? (
+								<Icon>chevron_right</Icon>
+							) : (
+								<Icon>chevron_left</Icon>
+							)}
+						</IconButton>
+					</div>
+					<Divider />
+					<Menus />
+				</Drawer>
+				<main
+					className={classNames(classes.content, classes[`content-left`], {
+						[classes.contentShift]: menuOpen,
+						[classes['contentShift-left']]: menuOpen
+					})}>
+					<div className={classes['content-top']} />
+					<Switch>
+						{elements &&
+							_.union(publicEles, elements).map<JSX.Element>(
+								(val): any => {
+									return (
+										<Route key={val.Name} path={'/front/' + val.Name}>
+											<MainComponent element={val} />
+										</Route>
+									);
+								}
+							)}
+						<Route key="not found" component={NotFound} />
+					</Switch>
+				</main>
+			</div>
+		);
+	}
+	private hideUserMenu(event: any) {
+		if (this.target.current && this.target.current.contains(event.target)) {
+			return;
+		}
+		if (this.props.userMenuOpen) {
+			this.props.toggleUserMenu(false);
+		}
+	}
+	private toggleUserMenu() {
+		this.props.toggleUserMenu(!this.props.userMenuOpen);
+	}
+	private logout() {
+		this.props.toggleUserMenu(false);
+		logout();
+	}
 }
 export default compose(
-    withStyles(styles, { withTheme: true }),
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )
+	withStyles(styles, { withTheme: true }),
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)
 )(Home);
--- main/home/style.ts
+++ main/home/style.ts
@@ -2,78 +2,79 @@ import { Theme } from '@material-ui/core';
 const drawerWidth = 260;
 export type clsNames = keyof ReturnType<typeof styles>;
 export const styles = (theme: Theme) => ({
-    toolbarText: {
-        flex: 1
-    },
-    appFrame: {
-        height: '100%',
+	toolbarText: {
+		flex: 1
+	},
+	appFrame: {
+		height: '100%',
 
-        zIndex: 1,
-        overflow: 'hidden',
-        display: 'flex',
-        width: '100%'
-    },
-    appBar: {
-        position: 'absolute' as 'absolute',
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        })
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen
-        })
-    },
-    menuButton: {
-        marginLeft: 12,
-        marginRight: 20
-    },
-    hide: {
-        display: 'none'
-    },
-    content: {
-        display: 'flex',
-        flexDirection: 'column' as 'column',
-        flexGrow: 1,
-
-        backgroundColor: theme.palette.background.default,
-        // padding: theme.spacing.unit * 3,
-        padding: 0,
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        })
-    },
-    'content-left': {
-        marginLeft: -drawerWidth
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen
-        })
-    },
-    'contentShift-left': {
-        marginLeft: 0
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '0 8px',
-        // backgroundColor: "rgba(0, 40, 77, 1)",
-        // color: "white",
-        ...theme.mixins.toolbar
-    },
-    drawerPaper: {
-        // backgroundColor: "rgb(0, 21, 41)",
-        position: 'relative' as 'relative',
-        width: drawerWidth
-    },
-    'content-top': {
-        ...theme.mixins.toolbar
-    }
+		zIndex: 1,
+		overflow: 'hidden',
+		display: 'flex',
+		width: '100%'
+	},
+	appBar: {
+		position: 'absolute' as 'absolute',
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		})
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	menuButton: {
+		marginLeft: 12,
+		marginRight: 20
+	},
+	hide: {
+		display: 'none'
+	},
+	content: {
+		display: 'flex',
+		flexDirection: 'column' as 'column',
+		flexGrow: 1,
+		width: '100%', // 必须要将宽度一层层下穿
+		backgroundColor: theme.palette.background.default,
+		// padding: theme.spacing.unit * 3,
+		padding: 0,
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		})
+	},
+	'content-left': {
+		marginLeft: -drawerWidth
+	},
+	contentShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	'contentShift-left': {
+		marginLeft: 0
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		padding: '0 8px',
+		// backgroundColor: "rgba(0, 40, 77, 1)",
+		// color: "white",
+		...theme.mixins.toolbar
+	},
+	drawerPaper: {
+		// backgroundColor: "rgb(0, 21, 41)",
+		position: 'relative' as 'relative',
+		width: drawerWidth
+	},
+	'content-top': {
+		...theme.mixins.toolbar
+	}
 });
